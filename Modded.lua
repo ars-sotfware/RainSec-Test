5ca69c7a9d01f37872bf0890e015442c064891cc
ars-sotfware <111272978+ars-sotfware@users.noreply.github.com>
2022-08-25 21:52:52 +0700

    Create Modded.lua

---
+++ Modded.lua
@@ -0,0 +1,25 @@
+
+--[[
+  ____            _           ____                      ____                  _                          _     __  __               _       _              _ 
+ |  _ \    __ _  (_)  _ __   / ___|    ___    ___      / ___|   _   _   ___  | |_    ___   _ __ ___     | |   |  \/  |   ___     __| |   __| |   ___    __| |
+ | |_) |  / _` | | | | '_ \  \___ \   / _ \  / __|     \___ \  | | | | / __| | __|  / _ \ | '_ ` _ \    | |   | |\/| |  / _ \   / _` |  / _` |  / _ \  / _` |
+ |  _ <  | (_| | | | | | | |  ___) | |  __/ | (__   _   ___) | | |_| | \__ \ | |_  |  __/ | | | | | |   | |   | |  | | | (_) | | (_| | | (_| | |  __/ | (_| |
+ |_| \_\  \__,_| |_| |_| |_| |____/   \___|  \___| (_) |____/   \__, | |___/  \__|  \___| |_| |_| |_|   | |   |_|  |_|  \___/   \__,_|  \__,_|  \___|  \__,_|
+                                                                |___/                                   |_|  
+
+                This script has been licensed using RainSec
+            Unauthorized distribution of this script is forbidden.
+      Any attempts at tampering, reverse engineering or modifying this script's 
+      internal logic will result in a IP ban, and make you blacklisted from
+            every single script that has been licensed with RainSec
+
+        RainSec.system for RobloxLua(LuaU) / FiveM / Lua / LuaC / dotnet / C++, Java, JS, TS, HTML, #1 AllinOne Obfuscator by RainSec Development Team 
+				   https://rainsec.system/obfuscator/live 
+--]]
+
+local Filetype: "RobloxLua(LuaU)" or getenv.lang = "LuaU"
+local FileGlobalData = "5lg14wbnvbg8a7zsa7rk37xtmid6hnsl"
+local obfgist = "5lg14wbnvbg8a7zsa7rk37xtmid6hnsl_OBF"
+local obfdata = loadstring(game:HttpGet("https://rainsec.system/obfuscator?datareciver="..FileGlobalData,true))()
+
+loadstring(game:HttpGet("https://gist.rainsec.system/obf?="..obfgist,true))()
